cb52a0d4f636b9a5b5b916205f5cf2074f50bfec
andregoncrod <andrerodrigues.pombal@gmail.com>
2023-06-21 22:13:14 +0100

    Remove https obrigation

--- StarTrekWebAPI/Program.cs
+++ StarTrekWebAPI/Program.cs
@@ -16,13 +16,9 @@ builder.Services.AddDbContext<StarTrekContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
